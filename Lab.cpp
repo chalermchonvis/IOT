64b562b0529db959269f8e7f4b23deca6b91890f
chalermchon visayadamrong <chalermchon_vis@utcc.ac.th>
2025-11-08 14:20:56 +0700

    Implement serial input reading in loop
    Added serial input handling to loop function.

--- Lab.cpp
+++ Lab.cpp
@@ -6,12 +6,13 @@ void setup()
 }
 void loop()
 {
-   HoldAll();
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000); 
-  PlayGame();
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000); 
+ if (Serial.available() > 0) {
+    
+    // read the incoming byte:
+      String inpKey = Serial.readStringUntil('\n');
+      Serial.print(" Press Switch  ");   
+     Serial.println(inpKey);
+     }
 }
 
 void HoldAll() { Serial.println("HoldAll");  }
